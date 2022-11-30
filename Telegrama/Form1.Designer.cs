8826d03be3f30308473adb3837a6f877a9f3b187
LucVerAmo <lucveramo@alu.edu.gva.es>
2022-11-30 15:53:30 +0100

    Botón
    Cambio del nombre del botón

--- Telegrama/Form1.Designer.cs
+++ Telegrama/Form1.Designer.cs
@@ -42,7 +42,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 47);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Calcular";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
